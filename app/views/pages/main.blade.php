e5e5dd81e8902aa79ff05f3eabdfb9c081bdace1
CafeCoder <coffeeshopcoder@users.noreply.github.com>
2014-09-29 01:08:27 +0900

    Added portfolio section.

--- app/views/pages/main.blade.php
+++ app/views/pages/main.blade.php
@@ -74,17 +74,62 @@
 	</div>
 </div>
 
- <div class="portfolio-section"></div>
-
- <div class="contact-section"></div>
-
- <footer class="footer-section section-root clearfix">
- 	<div class="company-info">
-	 	<h4>유한회사 플라잉비</h4>
-		서울시 금천구 가산동 493-6 대륭타운6차 10F, <br class="mobile-linebreak" />153-774<br />
-		Tel. 02 587 4351 Fax. 02 587 4352<br />
-		Email. contact@flyingbee.co.kr<br />
-		Blog. <a href="http://blog.naver.com/flying_bee" target="_blank">http://blog.naver.com/flying_bee</a><br />
+<div id="portfolioSection" class="portfolio-section section-root clearfix">
+	<div class="grid-wrapper row">
+		<div class="column col-xs-12 col-sm-4">
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/1.png')}}" />
+			</figure>
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/2.png')}}" />
+			</figure>
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/3.png')}}" />
+			</figure>
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/4.png')}}" />
+			</figure>
+		</div>
+		<div class="column tile-block col-xs-12 col-sm-4">
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/5.png')}}" />
+			</figure>
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/6.png')}}" />
+			</figure>
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/7.png')}}" />
+			</figure>
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/8.png')}}" />
+			</figure>
+		</div>
+		<div class="column tile-block col-xs-12 col-sm-4">
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/9.png')}}" />
+			</figure>
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/10.png')}}" />
+			</figure>
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/11.png')}}" />
+			</figure>
+			<figure class="portfolio-tile">
+				<img src="{{asset('assets/portfolio/12.png')}}" />
+			</figure>
+		</div>
 	</div>
- </footer>
+</div>
+
+<div class="contact-section"></div>
+
+<footer class="footer-section section-root clearfix">
+	<div class="company-info">
+	<h4>유한회사 플라잉비</h4>
+	서울시 금천구 가산동 493-6 대륭타운6차 10F, <br class="mobile-linebreak" />153-774<br />
+	Tel. 02 587 4351 Fax. 02 587 4352<br />
+	Email. contact@flyingbee.co.kr<br />
+	Blog. <a href="http://blog.naver.com/flying_bee" target="_blank">http://blog.naver.com/flying_bee</a><br />
+</div>
+</footer>
 @stop
